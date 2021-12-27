d24f9248f128a20995e6f4803cfb28fd57b4cdca
Hunar Ahmad <39065447+hunar4321@users.noreply.github.com>
2021-12-27 16:16:29 +0300

    Update ols_simple.py

--- ols_simple.py
+++ ols_simple.py
@@ -2,16 +2,19 @@ import numpy as np
 import matplotlib.pylab as plt
 import time
 
+#number of features (fet) and the number of labels (100)
 fet=29; lab=100;
 
+# generating time series data
 x = np.random.randn(fet,lab);
 bt = np.random.randn(1,fet)*10;
 
 y = np.dot(bt, x)
 
-## add some noise
+## adding some noise
 y = y+np.random.randn(1, lab)*0
 
+print("solving the proble using pinv and the existing libraries")
 t = time.time()
 pv = np.linalg.pinv(x) 
 Wm = y @ pv
@@ -21,42 +24,34 @@ print("Elapsed1:", time.time()-t)
 # plt.plot(bt[0])
 # plt.plot(Wm[0])
 
-
-def learn(xvec, yvec, sx):
-    kapa = np.dot(xvec, yvec)/sx
-    return kapa
-def predict(kapa, vec):
-    p = kapa * vec
-    return p
-
+print("solving using the simple method of regressing the errors out from each variable")
+t=time.time()
 tx=np.copy(x);
 ty=np.copy(y);
 k=1;
-
-t=time.time()
 sx=np.zeros(fet); 
 b =np.zeros(fet);
 for i in range(fet-1, -1, -1):
     sx[i] = np.dot(x[i,:], x[i, :])
     for j in range(0, fet-k):
-        kapa1 = learn(x[j, :], x[i, :], sx[i])
-        xhatj = predict(kapa1, x[i, :])
+        kapa = np.dot(x[j, :], x[i, :])/sx[i]
+        xhatj = kapa * x[i,:]
         x[j,:] = x[j, :] - xhatj
     k +=1
     
 for i in range(fet-1, 0, -1): 
-    kapa2 = learn(y, x[i,:], sx[i])
-    yhat = predict(kapa2, x[i,:])
+    kapa = np.dot(y, x[i, :])/sx[i]
+    yhat = kapa * x[i,:]
     y = y - yhat 
     
-b[0] = learn(y, x[0,:], sx[0]) 
+b[0] = np.dot(y, x[0,:])/sx[0]
 for i in range(fet-1):
     ty = ty - b[i]*tx[i,:]
-    b[i+1] = learn(ty, x[i+1,:], sx[i+1] )
-    
+    b[i+1] = np.dot(ty, x[i+1,:])/sx[i+1]
     
 print("Elapsed2:", time.time()-t)
 
+# Comparing the results with the true values
 plt.figure(2)
 plt.title("weights")
 plt.plot(bt[0]); plt.plot(Wm[0]); plt.plot(b);
@@ -71,73 +66,9 @@ plt.plot(y[0])
 plt.plot(ypred)
 
 
-### previous implimentations
-
-# tx=np.copy(x);
-# ty=np.copy(y);
-# k=1;
-
-# t=time.time()
-# sx=np.zeros(fet); 
-# b =np.zeros(fet);
-# for i in range(fet-1, -1, -1):
-#     sx[i] = np.dot(x[i,:], x[i, :])
-#     for j in range(0, fet-k):
-#         kapa1 = np.dot(x[j,:], x[i,:])/sx[i]
-#         xhatj = kapa1 * x[i,:]
-#         x[j,:] = x[j, :] - xhatj
-#     k +=1
-    
-# for i in range(fet-1, 0, -1):    
-#     kapa2 = np.dot(y, x[i,:])/sx[i] 
-#     yhat = kapa2 * x[i,:]
-#     y = y - yhat 
-    
-# b[0] = np.dot(y, x[0,:])/sx[0]   
-# for i in range(fet-1):
-#     ty = ty - b[i]*tx[i,:]
-#     b[i+1] = np.dot(ty, x[i+1,:])/sx[i+1] 
-    
-    
-# print("Elapsed2:", time.time()-t)
-
-# plt.figure(2)
-# plt.plot(bt[0]); plt.plot(Wm[0]); plt.plot(b);
 
 
 
-
-# ## simple working implimentation
-# ty=np.copy(y);
-# tx=np.copy(x);
-# yh=np.copy(ty);
-# k=1;
-
-# t=time.time()
-# sx=np.zeros(fet); 
-# b =np.zeros(fet);
-# ## iterate from last to first (except first element)
-# for i in range(fet-1, 0, -1):
-#     sx[i] = np.dot(x[i,:], x[i, :])
-#     kapa1 = np.dot(yh, x[i,:])/sx[i] 
-#     yhat = kapa1 * x[i,:]
-#     yh = yh - yhat 
-#     for j in range(0, fet-k):
-#         kapa2 = np.dot(x[j,:], x[i,:])/sx[i]
-#         xhat = kapa2 * x[i,:]
-#         x[j,:] = x[j, :] - xhat
-#     k +=1
-
-# b[0] = np.dot(yh, x[0,:])/np.dot(x[0,:], x[0,:])    
-# for i in range(fet-1):
-#     y = y - b[i]*tx[i,:]
-#     b[i+1] = np.dot(y, x[i+1,:])/np.dot(x[i+1], x[i+1])
-    
-    
-# print("Elapsed2:", time.time()-t)
-
-# plt.figure(2)
-# plt.plot(bt[0]); plt.plot(Wm[0]); plt.plot(b);
 
 
 
